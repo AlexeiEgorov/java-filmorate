cf9547113d2666f967789b5f7ae675e6e8312636
Alexei Egorov <zibbidizibbidibom@gmail.com>
2023-10-05 15:30:08 +0300

    feat: добавлена база данных, genre, mpa

--- src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.filmorate.dao.GenreDao;
 import ru.yandex.practicum.filmorate.model.Genre;
-import ru.yandex.practicum.filmorate.service.FilmService;
 
 import java.util.List;
 
