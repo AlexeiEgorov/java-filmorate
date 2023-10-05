4fa7c7b6b50832b22636855b41896bb237e21162
Alexei Egorov <zibbidizibbidibom@gmail.com>
2023-10-05 15:39:42 +0300

    feat: добавлена база данных, genre, mpa

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -6,7 +6,6 @@ import ru.yandex.practicum.filmorate.exception.EntityNotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
